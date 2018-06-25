194cfc659b843e6f7e015318771bdc0baf5c9efd
Bartosz Maciej Jablonka <bartosz.maciej.jablonka@gmail.com>
2018-06-26 00:27:11 +0200

    Add simple modular arithmetic

---
+++ ExtendedGaloisField.py
@@ -0,0 +1,4 @@
+from GaloisField import GaloisField
+
+
+print GaloisField.GaloisField(7,3)
---
+++ ModularArithmetic.py
@@ -0,0 +1,47 @@
+from Helper import *
+
+class ModularArithmetic:
+    def __init__(self, dec_value, modulus):
+        if isinstance(dec_value, int) and isinstance(modulus, int):
+            if dec_value >= 0 and modulus > 0: 
+                self.value = dec_value % modulus;
+                self.modulus = modulus;
+            else:
+                raise ValueError("Value and modulus have to be greater than zero")
+        else:
+            raise TypeError("Value and modulus have to be integers")
+
+    def __add__(self, other):
+        self.isOperationValid(other)
+        value = (self.value + other.value) % self.modulus
+        return ModularArithmetic(value, self.modulus)
+
+    def __mul__(self, other):
+        self.isOperationValid(other)
+        value = self.value * other.value
+        return ModularArithmetic(value, self.modulus)
+
+    def __invert__(self):
+        g, a, _ = egcd(self.value, self.modulus)
+        if g == 1:
+            return a%self.modulus
+        else:
+            print "Modular inverse does not exist!", self.value, "mod", self.modulus
+            return None
+
+    def __str__(self):
+        return str(self.value) + '%' + str(self.modulus)
+
+    def __unicode__(self):
+        return str(self.value) + '%' + str(self.modulus)
+
+    def isOperationValid(self, other):
+        """Validate operation"""
+        if isinstance(other, ModularArithmetic):
+            if self.modulus == other.modulus:
+                pass
+            else:
+                raise ValueError("Modules are different")
+        else:
+            raise TypeError("Second element is not a valid ModularArithmetic object")
+
